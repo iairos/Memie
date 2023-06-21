1a7daa55745a41017515a05d41f33519e071fc1a
iairos <iairschorr@gmail.com>
2023-06-21 15:33:41 +0300

    render a gallery

--- js/memie-controller.js
+++ js/memie-controller.js
@@ -1,1 +1,17 @@
 'use strict'
+function onInit() {
+  renderGallery()
+}
+
+function renderGallery() {
+  const images = getImgs()
+  const strHTMLs = images.map(
+    (img) =>
+      `      <article class="gallery-card">
+                <button title = "select" class = "btn gallery-btn" onclick="onSelectImg('${img.id}')">select</button>
+                <img src="${img.url}" alt="">
+             </article>`
+  )
+  const elGallery = document.querySelector('.gallery-container')
+  elGallery.innerHTML = strHTMLs.join('')
+}
